182c5db36e1525fbb46a0c160d663cc0dd5ca6a3
SivaMani <sivamani.r70@gmail.com>
2023-12-03 14:50:44 +0530

    Modified V1.0 with Coments -- V1.0.1

--- main.ps1
+++ main.ps1
@@ -1,4 +1,5 @@
 . .\key.ps1
+
 "
 ####################################################################################################
 This script is created by SivaMani to automate the IP reputation checking using the AbuseIPDB API.
@@ -11,7 +12,6 @@ Replace the {`$KEY} value with your API-Key
 
 # {$IPS} - is an array; which holds a list of IPs we need to check
 $IPS = ("157.230.80.76");
-
 $EndPoint = "https://api.abuseipdb.com/api/v2/check"
 
 # Headers for the endpoint
@@ -22,31 +22,24 @@ $Headers = @{
     "Accept" = "application/json"
 }
 
-
 # Header for the CSV file
 $CSV_Heading = "IPAddress, Whitelisted,ISP, AbuseConfidenceScore, Domain, IsTor, UsageType,  CountryCode"
 $Response_Data = New-Object System.Collections.ArrayList
 $index = 0
 
 Write-Output "Checking IPs reputation"
-
 foreach ($ipaddress in $IPS) {
-
     # URL parameters
     $Params = @{
         "ipAddress" = $ipaddress
     }
-
     try {
-
         $Response = Invoke-WebRequest -Uri $EndPoint -Method Get -Headers $Headers -Body $Params
-
         # Checking the response-status; if it is not 200 it will skip the current test
         if ($Response.StatusCode -ne 200) {
             Write-Output "Something went wrong;  Status Code: $($Response.StatusCode), Status Description: $($Response.StatusDescription)"
             return;
         }
-
         $Data = ($Response.content | ConvertFrom-Json).data
         $index = $Response_Data.Add("$($Data.ipAddress), $($Data.isWhitelisted), $($Data.isp), $($Data.abuseConfidenceScore),$($Data.domain), $($Data.isTor), $($Data.usageType), $($Data.countryCode)");
         Write-Output "Completed Checking $($index + 1) IP(s)"
@@ -54,8 +47,7 @@ foreach ($ipaddress in $IPS) {
     catch {
         Write-Output "Some error occured"
         $_.Exception.Response
-    }
-    
+    }  
 }
 
 # Creating a new out-put.csv file in the current folder with the current response data
