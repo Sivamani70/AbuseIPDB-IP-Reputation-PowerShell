efd7e44d9bbf61e382fd04c7f4f3677228f814c7
SivaMani <sivamani.r70@gmail.com>
2023-12-08 21:20:24 +0530

    Added Command line Argument - V1.1

--- main.ps1
+++ main.ps1
@@ -1,58 +1,72 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $FilePath
+)
+
+#Remove this . .key.ps1 and Replace the {`$KEY} value with your API-Key
 . .\key.ps1
 
-"
-####################################################################################################
-This script is created by SivaMani to automate the IP reputation checking using the AbuseIPDB API.
+if (!(Test-Path $FilePath)) {
+    Write-Error "$FilePath is not found in the current location"
+    return
+}
 
-To use this script -- Get your API key from AbuseIPDB.
-
-Replace the {`$KEY} value with your API-Key
-####################################################################################################
-"
-
-# {$IPS} - is an array; which holds a list of IPs we need to check
-$IPS = ("157.230.80.76");
-$EndPoint = "https://api.abuseipdb.com/api/v2/check"
-
-# Headers for the endpoint
-# Add your API-Key - Get the key from Abuseipdb
-# {$KEY} -- holds the API-Key value -- Key need to passed along with the request
-$Headers = @{
+# Constant - Values
+$ENDPOINT = "https://api.abuseipdb.com/api/v2/check"
+$HEADERS = @{
     "key"    = $KEY
     "Accept" = "application/json"
 }
-
 # Header for the CSV file
-$CSV_Heading = "IPAddress, Whitelisted,ISP, AbuseConfidenceScore, Domain, IsTor, UsageType,  CountryCode"
-$Response_Data = New-Object System.Collections.ArrayList
-$index = 0
+$CSV_FILE_HEADING = "IPAddress, Whitelisted,ISP, AbuseConfidenceScore, Domain, IsTor, UsageType,  CountryCode"
 
+$listOfIPs = New-Object System.Collections.ArrayList
+$responseData = New-Object System.Collections.ArrayList
+$basicPatteren = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+$content = Get-Content -Path $FilePath
+
+if ($content -eq " " -or $content.Length -eq 0) {
+    Write-Warning "No IPS found in the given file"
+    return
+}
+
+foreach ($ip in $content) {
+    $ip = $ip.Trim()
+    if ([System.Text.RegularExpressions.Regex]::IsMatch($ip, $basicPatteren)) {
+        $listOfIPS.Add($ip) | Out-Null
+    }
+}
+
+Write-Output "$($listOfIPs.Count) - IPs found in the file $FilePath"
 Write-Output "Checking IPs reputation"
-foreach ($ipaddress in $IPS) {
+
+foreach ($ipaddress in $listOfIPs) {
     # URL parameters
-    $Params = @{
+    $queryParameters = @{
         "ipAddress" = $ipaddress
     }
     try {
-        $Response = Invoke-WebRequest -Uri $EndPoint -Method Get -Headers $Headers -Body $Params
+        $response = Invoke-WebRequest -Uri $ENDPOINT -Method Get -Headers $HEADERS -Body $queryParameters
         # Checking the response-status; if it is not 200 it will skip the current test
-        if ($Response.StatusCode -ne 200) {
-            Write-Output "Something went wrong;  Status Code: $($Response.StatusCode), Status Description: $($Response.StatusDescription)"
+        if ($response.StatusCode -ne 200) {
+            Write-Output "Something went wrong;  Status Code: $($response.StatusCode), Status Description: $($response.StatusDescription)"
             return;
         }
-        $Data = ($Response.content | ConvertFrom-Json).data
-        $index = $Response_Data.Add("$($Data.ipAddress), $($Data.isWhitelisted), $($Data.isp), $($Data.abuseConfidenceScore),$($Data.domain), $($Data.isTor), $($Data.usageType), $($Data.countryCode)");
-        Write-Output "Completed Checking $($index + 1) IP(s)"
+        $data = ($response.content | ConvertFrom-Json).data
+        $responseData.Add("$($data.ipAddress), $($data.isWhitelisted), $($data.isp), $($data.abuseConfidenceScore),$($data.domain), $($data.isTor), $($data.usageType), $($data.countryCode)") | Out-Null
+        Write-Output "Completed Checking $($responseData.Count) IP(s)"
     }
     catch {
         Write-Output "Some error occured"
-        $_.Exception.Response
+        $PSItem
     }  
 }
 
 # Creating a new out-put.csv file in the current folder with the current response data
-Write-Output "$($index + 1)  IP(s) checked" 
+Write-Output "$($responseData.Count)  IP(s) checked" 
 Write-Output "Creating .\out-put.csv file"
-Set-Content -Path ".\out-put.csv" -Value $CSV_Heading
-Add-Content -Path ".\out-put.csv" -Value $Response_Data
+Set-Content -Path ".\out-put.csv" -Value $CSV_FILE_HEADING
+Add-Content -Path ".\out-put.csv" -Value $responseData
 Write-Output "Completed.!"    
